23da4c814f25dd29631472d210cccb8fea0a737d
Max <maxi.ng@hotmail.com>
2019-01-06 23:08:21 -0300

    console.table

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -1,8 +1,5 @@
 const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
-
-context.fillStyle = '#000';
-context.fillRect(0, 0, canvas.width, canvas.height);
 context.scale(20, 20);
 
 const matrix = [
@@ -11,12 +8,107 @@ const matrix = [
 	[0, 1, 0], 
 ];
 
-matrix.forEach((row, y) => { 
-	row.forEach((value, x) => {
-		if(value != 0) 
-		{
-			context.fillStyle = 'red';
-			context.fillRect(x, y, 1, 1);
-		}
+function draw()
+{
+	context.fillStyle = '#000';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	drawMatrix(player.matrix, player.pos);
+}
+
+function drawMatrix(matrix, offset)
+{
+	matrix.forEach( (row, y) =>
+	{ 
+		row.forEach( (value, x) => 
+		{ 
+			if(value != 0) 
+			{ 
+				context.fillStyle = 'red'; 
+				context.fillRect( x + offset.x, 
+								  y + offset.y, 
+								  1, 1); 
+			} 
+		}); 
 	});
+}
+
+function merge(arena, player)
+{
+	player.matrix.forEach((row, y) =>
+	{
+		row.forEach((value, x) => 
+		{
+			if(value !== 0)
+			{
+				arena[y + player.pos.y][x + player.pos.x] = value;
+			}
+		});
+	});
+}
+
+function playerDrop()
+{
+	player.pos.y++;
+	dropCounter = 0;
+}
+
+
+
+function createMatrix(w, h)
+{
+	const matrix = []
+	while(h--)
+	{
+		matrix.push(new Array(w).fill(0));
+	}
+	return matrix;
+}
+
+let dropCounter = 0;
+let dropInterval = 1000;
+let lastTime = 0;
+
+function update(time = 0)
+{
+	const deltaTime = time - lastTime;
+	lastTime = time;
+
+	dropCounter += deltaTime;
+	if(dropCounter > dropInterval)
+	{
+		player.pos.y++;
+		dropCounter = 0;
+	}
+	draw();
+	requestAnimationFrame(update);
+}
+
+document.addEventListener('keydown', event => 
+{
+	if(event.keyCode === 37)
+	{
+		player.pos.x--;
+	}
+	else if(event.keyCode == 39)
+	{
+		player.pos.x++;
+	}
+	else if(event.keyCode === 40)
+	{
+		playerDrop();
+		player.pos.y++;
+		dropCounter = 0;
+	}
+	//console.log(event);
 });
+
+const arena = createMatrix(12, 20);
+console.log(arena);
+//console.table(arena);
+
+const player = {
+	pos: {x: 5, y: 5},
+	matrix: matrix,
+};
+
+update();
