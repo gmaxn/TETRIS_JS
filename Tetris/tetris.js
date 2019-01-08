f03fa9a471cb0d6e6d46e3ef43019d7b1202cac6
Max <maxi.ng@hotmail.com>
2019-01-07 23:36:48 -0300

    -

--- Tetris/tetris.js
+++ Tetris/tetris.js
@@ -2,18 +2,112 @@ const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 context.scale(20, 20);
 
+function arenaSweep()
+{
+	outer: for(let y = arena.length -1; y > 0; --y)
+	{
+		for(let x = 0; x < arena[y].length; ++x)
+		{
+			if(arena[y][x] === 0)
+			{
+				continue outer;
+			}
+			const row = arena.splice(y, 1)[0].fill(0);
+			arena.unshift(row);
+			++y;
+		}
+	}
+}
 const matrix = [
 	[0, 0, 0],
 	[1, 1, 1],
 	[0, 1, 0], 
 ];
 
-function draw()
+function collide(arena, player)
 {
-	context.fillStyle = '#000';
-	context.fillRect(0, 0, canvas.width, canvas.height);
-	drawMatrix(player.matrix, player.pos);
+	const [matrix, offset] = [player.matrix, player.pos];
+
+	for (let y = 0; y < matrix.length; ++y)
+	{
+		for(let x = 0; x < matrix[y].length; ++x)
+		{
+			if(matrix[y][x] !== 0)
+			{
+				if((arena[y + offset.y] && arena[y + offset.y][x + offset.x]) !== 0)
+				{
+					return true;
+				}
+			}
+		}
+	}
+	return false;
 }
+
+function createMatrix(w, h)
+{
+	const matrix = [];
+	while(h--)
+	{
+		matrix.push(new Array(w).fill(0));
+	}
+	return matrix;
+}
+
+function createPiece(type)
+{
+	if(type === 'T')
+	{
+		return [
+		[0, 0, 0],
+		[1, 1, 1],
+		[0, 1, 0], ];
+	}
+	else if(type === 'O')
+	{
+		return [
+		[2, 2],
+		[2, 2], ];
+	}
+	else if(type === 'L')
+	{
+		return [
+		[0, 3, 0],
+		[0, 3, 0],
+		[0, 3, 3], ];
+	}
+	else if(type === 'J')
+	{
+		return [
+		[0, 4, 0],
+		[0, 4, 0],
+		[4, 4, 0], ];
+	}
+	else if(type === 'I')
+	{
+		return [
+		[0, 5, 0, 0],
+		[0, 5, 0, 0],
+		[0, 5, 0, 0],
+		[0, 5, 0, 0], ];
+	}
+	else if(type === 'S')
+	{
+		return [
+		[0, 6, 6],
+		[6, 6, 0],
+		[0, 0, 0], ];
+	}
+	else if(type === 'Z')
+	{
+		return [
+		[7, 7, 0],
+		[0, 7, 7],
+		[0, 0, 0], ];
+	}
+
+}
+
 
 function drawMatrix(matrix, offset)
 {
@@ -23,13 +117,23 @@ function drawMatrix(matrix, offset)
 		{ 
 			if(value != 0) 
 			{ 
-				context.fillStyle = 'red'; 
+				context.fillStyle = colors[value];
 				context.fillRect( x + offset.x, 
 								  y + offset.y, 
 								  1, 1); 
 			} 
 		}); 
 	});
+}
+
+function draw()
+{
+	context.fillStyle = '#000';
+	context.fillRect(0, 0, canvas.width, canvas.height);
+
+	drawMatrix(arena, {x: 0, y: 0});
+	drawMatrix(player.matrix, player.pos);
+
 }
 
 function merge(arena, player)
@@ -49,19 +153,70 @@ function merge(arena, player)
 function playerDrop()
 {
 	player.pos.y++;
+
+
+	if(collide(arena, player))
+	{
+		player.pos.y--;
+		merge(arena, player);
+		playerReset();
+		arenaSweep();
+	}
 	dropCounter = 0;
 }
 
-
-
-function createMatrix(w, h)
+function playerMove(dir)
 {
-	const matrix = []
-	while(h--)
+	player.pos.x += dir;
+	if(collide(arena, player))
 	{
-		matrix.push(new Array(w).fill(0));
+		player.pos.x -= dir;
 	}
-	return matrix;
+}
+
+function playerReset()
+{
+	const pieces = 'ILJOTSZ';
+	player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
+	player.pos.y = 0;
+	player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
+	if(collide(arena, player))
+	{
+		arena.forEach(row => row.fill(0));
+	}
+}
+function playerRotate(dir)
+{
+	const pos = player.pos.x;
+	let offset = 1;
+	rotate(player.matrix, dir);
+	while(collide(arena, player))
+	{
+		player.pos.x+=offset;
+		offset = -(offset + (offset > 0 ? 1 : -1));
+		if(offset > player.matrix[0].length)
+		{
+			rotate(player.matrix, -dir);
+			player.pos.x = pos;
+			return;
+		}
+	}
+}
+
+function rotate(matrix, dir)
+{
+	for(let y = 0; y < matrix.length; ++y)
+	{
+		for(let x = 0; x < y; ++x)
+		{
+			[matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]]; 
+		}
+	}
+
+	if(dir > 0)
+		matrix.forEach(row => row.reverse());
+	else
+		matrix.reverse();
 }
 
 let dropCounter = 0;
@@ -76,35 +231,50 @@ function update(time = 0)
 	dropCounter += deltaTime;
 	if(dropCounter > dropInterval)
 	{
-		player.pos.y++;
-		dropCounter = 0;
+		playerDrop();
 	}
 	draw();
 	requestAnimationFrame(update);
-}
 
+}
+const colors = [
+	null,
+	'#E32468',
+	'#98E22B',
+	'#67D8EF',
+	'#E79622',
+	'#7B76FF',
+	'#90918B',
+	'#F8F8F2',
+]
 document.addEventListener('keydown', event => 
 {
 	if(event.keyCode === 37)
 	{
-		player.pos.x--;
+		playerMove(-1);		
 	}
-	else if(event.keyCode == 39)
+	else if(event.keyCode === 39)
 	{
-		player.pos.x++;
+		playerMove(+1);
+	}
+	else if(event.keyCode === 32)
+	{
+		playerDrop();
+	}
+	else if(event.keyCode === 38)
+	{
+		playerRotate(-1);
 	}
 	else if(event.keyCode === 40)
 	{
-		playerDrop();
-		player.pos.y++;
-		dropCounter = 0;
+		playerRotate(+1);
 	}
 	//console.log(event);
 });
 
 const arena = createMatrix(12, 20);
-console.log(arena);
-//console.table(arena);
+//console.log(arena);
+
 
 const player = {
 	pos: {x: 5, y: 5},
